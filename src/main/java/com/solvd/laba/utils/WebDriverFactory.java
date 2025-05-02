8dbb11693d4489cab3bde5befaac8f71845620d6
Olesya Kolenchenko <kololesya@gmail.com>
2025-05-02 14:00:18 +0200

    refactoring WebDriverFactory

--- src/main/java/com/solvd/laba/utils/WebDriverFactory.java
+++ src/main/java/com/solvd/laba/utils/WebDriverFactory.java
@@ -2,11 +2,8 @@ package com.solvd.laba.utils;
 
 import java.net.URL;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.slf4j.Logger;
@@ -19,12 +16,11 @@ public class WebDriverFactory {
     private static final Logger logger = LoggerFactory.getLogger(WebDriverFactory.class);
 
     public static WebDriver getDriver(String browser) {
-        EnvironmentConfig config = new EnvironmentConfig();
-        String runMode = config.getProperty("run.mode", "local").toLowerCase();
-
-        try {
-            if (runMode.equals("grid")) {
+        if (driver.get() == null) {
+            try {
+                EnvironmentConfig config = new EnvironmentConfig();
                 URL gridUrl = new URL(config.getProperty("selenium.grid.url"));
+
                 switch (browser.toLowerCase()) {
                     case "chrome" -> {
                         ChromeOptions options = new ChromeOptions();
@@ -38,28 +34,12 @@ public class WebDriverFactory {
                     }
                     default -> throw new RuntimeException("Unsupported browser: " + browser);
                 }
-            } else {
-                switch (browser.toLowerCase()) {
-                    case "chrome" -> {
-                        WebDriverManager.chromedriver().setup();
-                        driver.set(new ChromeDriver());
-                        logger.info("Local Chrome launched");
-                    }
-                    case "firefox" -> {
-                        WebDriverManager.firefoxdriver().setup();
-                        driver.set(new FirefoxDriver());
-                        logger.info("Local Firefox launched");
-                    }
-                    default -> throw new RuntimeException("Unsupported browser: " + browser);
-                }
+            } catch (Exception e) {
+                throw new RuntimeException("Failed to initialize RemoteWebDriver", e);
             }
-        } catch (Exception e) {
-            throw new RuntimeException("Driver initialization failed", e);
         }
-
         return driver.get();
     }
-
 
     public static void quitDriver() {
         if (driver.get() != null) {
